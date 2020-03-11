0b9ce3f72fb6f5a6b1209a9f2bd9f2824769490f
abyester <26520858+abyester@users.noreply.github.com>
2020-03-11 11:44:25 +0000

    Selecting channels finally working

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
@@ -8,12 +8,16 @@ app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
 usernames = []
+channels = ["javascript", "babes"]
+messages = {}
+messages["javascript"] = [("18.42", "george", "Howzit in there"), ("19.45", "adam", "I don't know how to programme")]
+
 
 @app.route("/", methods=['GET'])
 def index():
     return render_template('index.html')
 
-@app.route("/user/<username>", methods=['POST'])
+@app.route("/users/<username>", methods=['POST'])
 def check_username(username):
     print("Entering check whether " + username + " is in usernames")
     if username in usernames:
@@ -23,3 +27,14 @@ def check_username(username):
         print("Username does not already exist")
         usernames.append(username)
         return "valid"
+
+@app.route("/users/<username>/<channel>", methods=['POST'])
+def open_channel(username, channel):
+    if channel not in channels:
+       channels.append(channel)
+    
+    return jsonify(messages[channel])
+
+@app.route("/channellist", methods=['POST'])
+def list_channels():
+    return jsonify(channels)
